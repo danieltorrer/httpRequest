d5133c566c7f46567962cf9eed2c24479e1e8bec
iMac <imac@danieltorrer.local>
2018-07-23 16:01:01 -0500

    a post

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
 
+
+  ngOnInit() {
+    this.http
+      .post('${ this.api.API_URL }user / login', {
+        usr: 'this.login.usr',
+        pword: 'this.login.pword'
+      })
+      .subscribe(response => {
+        console.log(response)
+      });
+  }
 }
